aa490e6a1e0bcdc99fd2399c2263cb850f8d390f
黄兵 <1278032416@qq.com>
2019-09-23 13:38:31 +0800

    spring boot 组件化开发 @paramsNotNull

--- spring-boot-test/src/main/java/cn/htool/test/Application.java
+++ spring-boot-test/src/main/java/cn/htool/test/Application.java
@@ -14,6 +14,6 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 @EnableAspectJAutoProxy
 public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class,args);
+        SpringApplication.run(Application.class, args);
     }
 }
