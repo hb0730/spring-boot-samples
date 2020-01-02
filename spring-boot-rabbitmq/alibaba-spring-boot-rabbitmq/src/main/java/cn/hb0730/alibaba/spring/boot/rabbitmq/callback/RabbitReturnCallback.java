c6cb96905f9117b87f8c241fa0ee5f10b0662b38
黄兵 <1278032416@qq.com>
2020-01-02 10:41:31 +0800

    add rabbitmq

---
+++ spring-boot-rabbitmq/alibaba-spring-boot-rabbitmq/src/main/java/cn/hb0730/alibaba/spring/boot/rabbitmq/callback/RabbitReturnCallback.java
@@ -0,0 +1,22 @@
+package cn.hb0730.alibaba.spring.boot.rabbitmq.callback;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+
+/**
+ * <p>
+ * </P>
+ *
+ * @author bing_huang
+ * @since V1.0
+ */
+public class RabbitReturnCallback implements RabbitTemplate.ReturnCallback {
+
+    Logger log= LoggerFactory.getLogger(RabbitReturnCallback.class);
+    @Override
+    public void returnedMessage(Message message, int replyCode, String replyText, String exchange, String routingKey) {
+        log.info("message return message:"+message+",replyCode:"+replyCode+",replyText:"+replyText+",exchange:"+exchange+",routingKey:"+routingKey);
+    }
+}
---
+++ spring-boot-rabbitmq/alibaba-spring-boot-rabbitmq/src/main/java/cn/hb0730/alibaba/spring/boot/rabbitmq/config/RabbitConfig.java
@@ -0,0 +1,79 @@
+package cn.hb0730.alibaba.spring.boot.rabbitmq.config;
+
+import cn.hb0730.alibaba.spring.boot.rabbitmq.provider.AliyunCredentialsProvider;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.amqp.RabbitProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * <p>
+ * </P>
+ *
+ * @author bing_huang
+ * @since V1.0
+ */
+@Configuration
+public class RabbitConfig {
+    //实例Id 从阿里云AMQP控制台获取
+    private static final String INSTANCE_ID = "1317287729996725";
+    @Autowired
+    private RabbitProperties rabbitProperties;
+
+
+    @Bean
+    public ConnectionFactory getConnectionFactory() {
+        com.rabbitmq.client.ConnectionFactory rabbitConnectionFactory =
+                new com.rabbitmq.client.ConnectionFactory();
+        rabbitConnectionFactory.setHost(rabbitProperties.getHost());
+        rabbitConnectionFactory.setPort(rabbitProperties.getPort());
+        rabbitConnectionFactory.setVirtualHost(rabbitProperties.getVirtualHost());
+
+        AliyunCredentialsProvider credentialsProvider = new AliyunCredentialsProvider(
+                rabbitProperties.getUsername(), rabbitProperties.getPassword(), INSTANCE_ID);
+        rabbitConnectionFactory.setCredentialsProvider(credentialsProvider);
+        rabbitConnectionFactory.setAutomaticRecoveryEnabled(true);
+        rabbitConnectionFactory.setNetworkRecoveryInterval(5000);
+        ConnectionFactory connectionFactory = new CachingConnectionFactory(rabbitConnectionFactory);
+        ((CachingConnectionFactory) connectionFactory).setPublisherConfirms(rabbitProperties.isPublisherConfirms());
+        ((CachingConnectionFactory) connectionFactory).setPublisherReturns(rabbitProperties.isPublisherReturns());
+        return connectionFactory;
+    }
+
+    /**
+     * 申明队列
+     *
+     * @return
+     */
+    @Bean
+    public Queue queue() {
+        Map<String, Object> arguments = new HashMap<>(4);
+
+        return new Queue("queue-rabbit-springboot-advance5", true, false, false, arguments);
+    }
+
+    @Bean
+    public Exchange exchange() {
+        Map<String, Object> arguments = new HashMap<>(4);
+
+        return new DirectExchange("exchange-rabbit-springboot-advance5", true, false, arguments);
+    }
+
+    /**
+     * 申明绑定
+     *
+     * @return
+     */
+    @Bean
+    public Binding binding() {
+        return BindingBuilder.bind(queue()).to(exchange()).with("product").noargs();
+    }
+
+}
+
