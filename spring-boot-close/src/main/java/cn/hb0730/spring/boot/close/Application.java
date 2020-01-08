eb14936c2105f345022448626387884a03435397
黄兵 <1278032416@qq.com>
2020-01-08 09:28:33 +0800

    add spring boot closed

---
+++ spring-boot-close/src/main/java/cn/hb0730/spring/boot/close/Application.java
@@ -0,0 +1,19 @@
+package cn.hb0730.spring.boot.close;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>
+ * </P>
+ *
+ * @author bing_huang
+ * @since V1.0
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
